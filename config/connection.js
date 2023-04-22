f6c84286e20864ab8bbae0135f9b3055c4792d52
Jose Orozco <josearozco15@gmail.com>
2023-04-22 14:44:25 -0600

    bugs

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,27 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
+const { config } = require('dotenv');
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL, // Use the DATABASE_URL provided by Railway
-  {
-    dialect: 'mysql',
-    protocol: 'mysql',
-    logging: true, // Set to true to log SQL queries
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false // Disable SSL verification for now
-      }
+config();
+
+let sequelize;
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: true
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.MYSQL_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      logging: true
     }
-  }
-);
+  );
+}
 
 module.exports = sequelize;
-
 
