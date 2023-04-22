5ace6dad1f396eb17d665d5c007a69f93ba57cdf
Jose Orozco <josearozco15@gmail.com>
2023-04-22 13:51:50 -0600

    debug

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,14 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
+const { config } = require('dotenv');
 
-let sequelize;
+config();
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.MYSQL_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
+let sequelize = new Sequelize(MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD, {
+  host: MYSQL_HOST,
+  dialect: 'mysql'
+});
 
 module.exports = sequelize;
+
 
